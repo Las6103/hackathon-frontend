1ac972335b80ae99150b5b5a73edf471c834d2f6
LeonardoSilva <apx88.0@gmail.com>
2020-06-26 10:54:26 -0400

    created articlelist

---
+++ hack/src/Articlelist.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Articlelist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        data = []
+    }
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Articlelist;
